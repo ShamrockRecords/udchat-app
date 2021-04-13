0df4bb56fc40b68c3bef955af3d4d4d9652dfd65
青木秀仁 <shamrock1976@me.com>
2021-04-13 12:28:11 +0900

    パラメータを環境変数にセット

--- app.js
+++ app.js
@@ -89,7 +89,18 @@ firebase.initializeApp(firebaseConfig);
 
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./firebase-adminsdk.json");
+var serviceAccount = {
+  "type": process.env.FIREBASE_ADMINSDK_type,
+  "project_id": process.env.FIREBASE_ADMINSDK_project_id,
+  "private_key_id": process.env.FIREBASE_ADMINSDK_private_key_id,
+  "private_key": process.env.FIREBASE_ADMINSDK_private_key,
+  "client_email": process.env.FIREBASE_ADMINSDK_client_email,
+  "client_id": process.env.FIREBASE_ADMINSDK_client_id,
+  "auth_uri": process.env.FIREBASE_ADMINSDK_auth_uri,
+  "token_uri": process.env.FIREBASE_ADMINSDK_token_uri,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_ADMINSDK_auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.FIREBASE_ADMINSDK_client_x509_cert_url,
+} ;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
