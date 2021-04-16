f74e50b7ef5278e480ada8bc88c403d01e967708
青木秀仁 <>
2021-04-16 15:15:03 +0900

    音声入力を追加

--- public/javascripts/language_support.js
+++ public/javascripts/language_support.js
@@ -35,8 +35,6 @@ async function translate(apiKey, message, from, to, completion) {
         URL += "&target=" + to + "&q=" + encodeURIComponent(message) ;
     
         let data = await fetch(URL).then(response => response.json()) ;
-       
-        console.log(data) ;
         
         try {
             completion(data.data.translations[0].translatedText, data.data.translations[0].detectedSourceLanguage) ;
