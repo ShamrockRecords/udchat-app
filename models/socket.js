c853ba3428fca8d686b5750afb05c952579fb03f
青木秀仁 <>
2021-04-20 14:45:47 +0900

    修正

--- models/socket.js
+++ models/socket.js
@@ -15,85 +15,131 @@ class socketWrapper {
 
         io.on('connection', (socket) => {
             socket.on("message", async (msg) => {
-                let command = JSON.parse(msg) ;
+                let commands = JSON.parse(msg) ;
 
-                if (command.method == 'requestConnecting') {
-                    if (this.socketsInstances[socket.id] == null) {
-                        let commands = [] ;
+                if (commands.length == undefined) {
+                    commands = [] ;
+                    commands.push(JSON.parse(msg)) ;
+                }
 
-                        let snapshot = await admin.firestore().collection("chat").doc(command.requestId).collection("messages").orderBy('timestamp', 'desc').limit(10).get() ;
+                for (let key in commands) {
+                    let command = commands[key] ;
+                
+                    if (command.method == 'requestConnecting') {
+                        if (this.socketsInstances[socket.id] == null) {
+                            let commands = [] ;
 
-                        for (let key in snapshot.docs) {
-                            let doc = snapshot.docs[key] ;
-                            let command = doc.data() ;
+                            let snapshot = await admin.firestore().collection("chat").doc(command.requestId).collection("messages").orderBy('timestamp', 'desc').limit(10).get() ;
 
-                            try {
-                                command.timestamp = command.timestamp.toDate().toUTCString() ;
-                            } catch(e) {
+                            for (let key in snapshot.docs) {
+                                let doc = snapshot.docs[key] ;
+                                let command = doc.data() ;
 
+                                try {
+                                    command.timestamp = command.timestamp.toDate().toUTCString() ;
+                                } catch(e) {
+
+                                }
+
+                                command.needsTranslating = false ;
+                                
+                                commands.push(command) ;
                             }
 
-                            command.needsTranslating = false ;
-                            
+                            commands = commands.reverse() ;
+
                             commands.push(command) ;
+
+                            let ids = await io.in(command.requestId).allSockets() ;
+
+                            for (let socketId of ids) {
+                                let command = this.socketsInstances[socketId] ;
+                                commands.push(this.createStatusCommand('joined', command)) ;
+                            }
+
+                            io.to(socket.id).emit('message', JSON.stringify(commands));
+                        }
+                    } else {
+                        this.socketsInstances[socket.id] = command ;
+                        socket.join(command.requestId);
+
+                        command["messageId"] = uuid.v4() ;
+                        command["timestamp"] = new Date() ;
+
+                        if (command.method != 'status') {
+                            await admin.firestore().collection("chat").doc(command.requestId).collection("messages").doc(command.messageId).set(command) ;
+                        }
+                        
+                        try {
+                            command.timestamp = command.timestamp.toDate().toUTCString() ;
+                        } catch(e) {
+
                         }
 
-                        commands = commands.reverse() ;
+                        command.needsTranslating = true ;
 
-                        commands.push(command) ;
-
-                        io.to(socket.id).emit('message', JSON.stringify(commands));
-                    } 
-
-                    return ;
+                        io.to(command.requestId).emit("message", JSON.stringify([command]));
+                    }
                 }
-
-                this.socketsInstances[socket.id] = command ;
-                socket.join(command.requestId);
-
-                command["messageId"] = uuid.v4() ;
-                command["timestamp"] = new Date() ;
-
-                if (command.method != 'status') {
-                    await admin.firestore().collection("chat").doc(command.requestId).collection("messages").doc(command.messageId).set(command) ;
-                }
-                
-                try {
-                    command.timestamp = command.timestamp.toDate().toUTCString() ;
-                } catch(e) {
-
-                }
-
-                command.needsTranslating = true ;
-
-                io.to(command.requestId).emit("message", JSON.stringify([command])); 
             }) ;
 
             socket.on("disconnect", async () => {
-                let command = this.socketsInstances[socket.id] ;
+                let lastCommand = this.socketsInstances[socket.id] ;
 
-                if (command != null) {
-                    command.message = command.name + 'が退出しました。' ;
+                if (lastCommand != null) {
+                    let commands = [] ;
 
-                    command["messageId"] = uuid.v4() ;
-                    command["timestamp"] = new Date() ;
-                    command["method"] = 'information' ;
-                    command["param"] = 'left' ;
-   
-                    await admin.firestore().collection("chat").doc(command.requestId).collection("messages").doc(command.messageId).set(command) ;
+                    {
+                        let command = {
+                            requestId: lastCommand.requestId,
+                            uid: lastCommand.uid,
+                            name: lastCommand.name,
+                            language: lastCommand.language,
+                            message: lastCommand.name + 'が退出しました。',
+                            method: 'information',
+                        } ;
 
-                    try {
-                        command.timestamp = command.timestamp.toDate().toUTCString() ;
-                    } catch(e) {
+                        command["messageId"] = uuid.v4() ;
+                        command["timestamp"] = new Date() ;
+    
+                        await admin.firestore().collection("chat").doc(command.requestId).collection("messages").doc(command.messageId).set(command) ;
 
+                        try {
+                            command.timestamp = command.timestamp.toDate().toUTCString() ;
+                        } catch(e) {
+
+                        }
+
+                        commands.push(command) ;
                     }
-        
-                    await io.to(command.requestId).emit("message", JSON.stringify([command])); 
+    
+                    {
+                        let command = this.createStatusCommand('left', lastCommand) ;
+
+                        command["messageId"] = uuid.v4() ;
+                        command["timestamp"] = new Date() ;
+
+                        commands.push(command) ;
+                    }
+
+                    await io.to(lastCommand.requestId).emit("message", JSON.stringify(commands)); 
 
                     delete  this.socketsInstances[socket.id] ;
                 }
             });
         });
+    }
+
+    createStatusCommand(status, copiedCommand) {
+        return {
+            requestId: copiedCommand.requestId,
+            uid: copiedCommand.uid,
+            name: copiedCommand.name,
+            language: '',
+            message: '',
+            method: 'status',
+            param: status
+        } ;
     }
 };
 
