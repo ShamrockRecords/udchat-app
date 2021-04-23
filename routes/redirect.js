a06b0dd10fcb86e30d9376f29f4b523e0065c0a1
青木秀仁 <shamrock1976@me.com>
2021-04-23 14:39:29 +0900

    テスト

--- routes/redirect.js
+++ routes/redirect.js
@@ -2,8 +2,10 @@ var express = require('express');
 let admin = require('firebase-admin');
 let firebaseSession = require('../models/firebase_session.js') ;
 var router = express.Router() ;
+var fetch = require('node-fetch') ;
+let Base64 = require('js-base64');
 
-router.post('/', async function(req, res, next) {
+router.get('/', async function(req, res, next) {
     let result = await firebaseSession.enter(req, res) ;
 
     if (!result) {
@@ -11,8 +13,15 @@ router.post('/', async function(req, res, next) {
         return ;
     }
 
-    res.write(JSON.stringify(req.body)) ;
-    res.write(JSON.stringify(req.query)) ;
+    let token = Base64.encode('RjVhuFOlQ5C5TBe_eraM_A' + ':' + process.env.ZOOM_CLIENT_SECRET);
+    
+    let URL = "https://zoom.us/oauth/token?grant_type=authorization_code&code={" + req.query.code + "}&redirect_uri=$Redirect_URL" ;
+
+    let data = await fetch(URL, {
+        method: "POST",
+        headers: {'authorization': 'Bearer: ' + token}}).then(response => response.json()) ;
+
+    res.write(JSON.stringify(data)) ;
     res.end() ;	 
 }) ;
 
