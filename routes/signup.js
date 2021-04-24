6a7c049bbe9a8aa1edc99b6c9f84c565fdfb0f03
青木秀仁 <>
2021-04-24 18:08:35 +0900

    サインアップ画面にもSNSログインのボタンを

--- routes/signup.js
+++ routes/signup.js
@@ -13,11 +13,21 @@ router.get('/', async function(req, res, next) {
 
 	let chatId = req.query.chatId ;
 
+	var config = {
+		apiKey: process.env.OPENED_FIREBASE_API_KEY,
+		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+		appId: process.env.FIREBASE_APP_ID,
+		measurementId: process.env.MEASUREMENT_ID
+	};
+	
 	res.render('signup', {
 		lang: res.locale,
-		title: 'サインアップ',
 		email: '',
 		chatId: chatId != undefined ? chatId : '',
+		config: config,
 		errorMessage: '',
 	});
 });
